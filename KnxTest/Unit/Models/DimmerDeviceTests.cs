411929b7d94b8689425b0311fb7808a19ac6a3dd
donadamus <adam.bastrzyk@gmail.com>
2025-08-09 00:17:21 +0200

    dimmer tests

--- KnxTest/Unit/Models/DimmerDeviceTests.cs
+++ KnxTest/Unit/Models/DimmerDeviceTests.cs
@@ -1,18 +1,22 @@
 using FluentAssertions;
 using KnxModel;
+using System;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace KnxTest.Unit.Models
 {
     public class DimmerDeviceTests : LightDeviceTestsBase<DimmerDevice, DimmerAddresses>
     {
         protected override DimmerDevice _device { get; }
-        // This class is just a placeholder to allow the test class to compile
-        // Actual tests are defined in DimmerDeviceTests
+
         public DimmerDeviceTests()
         {
             // Initialize DimmerDevice with mock KNX service
             _device = new DimmerDevice("D_TEST", "Test Dimmer", "1", _mockKnxService.Object);
         }
+
+        #region IKnxDeviceBase Tests
 
         [Fact]
         public override void Constructor_SetsBasicProperties()
@@ -21,6 +25,264 @@ namespace KnxTest.Unit.Models
             _device.Id.Should().Be("D_TEST");
             _device.Name.Should().Be("Test Dimmer");
             _device.SubGroup.Should().Be("1");
+            _device.LastUpdated.Should().Be(DateTime.MinValue); // Not initialized yet
         }
+
+        #endregion
+
+        #region IPercentageControllable Tests
+
+        [Fact]
+        public async Task SetPercentageAsync_ShouldSendCorrectTelegram()
+        {
+            // TODO: Test that SetPercentageAsync sends correct percentage value to dimming control address
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Theory]
+        [InlineData(0)]   // Off (minimum brightness)
+        [InlineData(1)]   // Minimum dimming level
+        [InlineData(25)]  // Quarter brightness
+        [InlineData(50)]  // Half brightness
+        [InlineData(75)]  // Three quarters brightness
+        [InlineData(100)] // Maximum brightness
+        public async Task SetPercentageAsync_WithValidValues_ShouldSendCorrectTelegram(byte percentage)
+        {
+            // TODO: Test SetPercentageAsync with various valid percentage values for dimming
+            // Parameter: percentage
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Theory]
+        [InlineData(101)] // Above maximum
+        [InlineData(255)] // Byte maximum
+        public async Task SetPercentageAsync_WithInvalidValues_ShouldThrowException(byte percentage)
+        {
+            // TODO: Test that SetPercentageAsync throws exception for invalid percentage values
+            // Parameter: percentage
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Fact]
+        public async Task DimToMaxAsync_ShouldSendCorrectTelegram()
+        {
+            // TODO: Test that DimToMaxAsync sends 100% to dimming control address
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Fact]
+        public async Task DimToMinAsync_ShouldSendCorrectTelegram()
+        {
+            // TODO: Test that DimToMinAsync sends 0% to dimming control address
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Theory]
+        [InlineData(5)]   // Small increment
+        [InlineData(10)]  // Standard increment
+        [InlineData(25)]  // Large increment
+        public async Task IncreaseBrightnessAsync_ShouldSendCorrectTelegram(byte increment)
+        {
+            // TODO: Test that IncreaseBrightnessAsync increases brightness by specified amount
+            // Parameter: increment
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Theory]
+        [InlineData(5)]   // Small decrement
+        [InlineData(10)]  // Standard decrement
+        [InlineData(25)]  // Large decrement
+        public async Task DecreaseBrightnessAsync_ShouldSendCorrectTelegram(byte decrement)
+        {
+            // TODO: Test that DecreaseBrightnessAsync decreases brightness by specified amount
+            // Parameter: decrement
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        #endregion
+
+        #region Dimmer-Specific Feedback Processing Tests
+
+        [Theory]
+        [InlineData(0)]   // Off
+        [InlineData(1)]   // Minimum brightness
+        [InlineData(25)]  // Quarter brightness
+        [InlineData(50)]  // Half brightness
+        [InlineData(75)]  // Three quarters brightness
+        [InlineData(100)] // Maximum brightness
+        public void OnPercentageFeedback_ShouldUpdateState(byte expectedPercentage)
+        {
+            // TODO: Test that percentage feedback updates CurrentPercentage property
+            // Parameter: expectedPercentage
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Fact]
+        public void OnPercentageFeedback_WhenLocked_ShouldStillUpdateState()
+        {
+            // TODO: Test that percentage feedback updates state even when device is locked
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Fact]
+        public void OnPercentageFeedback_ShouldNotAffectSwitchState()
+        {
+            // TODO: Test that percentage feedback only affects percentage, not switch state
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Fact]
+        public void OnSwitchFeedback_ShouldNotAffectPercentageState()
+        {
+            // TODO: Test that switch feedback only affects switch state, not percentage
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        #endregion
+
+        #region Dimmer-Specific State Reading Tests
+
+        [Fact]
+        public async Task ReadPercentageAsync_ShouldRequestCorrectAddress()
+        {
+            // TODO: Test that ReadPercentageAsync calls RequestGroupValue with dimming feedback address
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Theory]
+        [InlineData(0)]   // Off
+        [InlineData(50)]  // Half brightness
+        [InlineData(100)] // Full brightness
+        public async Task ReadPercentageAsync_ShouldReturnCorrectValue(byte expectedPercentage)
+        {
+            // TODO: Test ReadPercentageAsync returns correct percentage value
+            // Parameter: expectedPercentage
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Fact]
+        public async Task ReadPercentageAsync_ShouldUpdateCurrentPercentage()
+        {
+            // TODO: Test that ReadPercentageAsync updates CurrentPercentage property
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Fact]
+        public async Task ReadPercentageAsync_ShouldUpdateLastUpdated()
+        {
+            // TODO: Test that ReadPercentageAsync updates LastUpdated timestamp
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Fact]
+        public async Task ReadPercentageAsync_WhenKnxServiceThrows_ShouldPropagateException()
+        {
+            // TODO: Test that ReadPercentageAsync propagates KNX service exceptions
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        #endregion
+
+        #region Dimmer-Specific Wait Methods Tests
+
+        [Theory]
+        [InlineData(0, 100, 0, 50)]   // Wait for 0% (off)
+        [InlineData(50, 200, 0, 50)]  // Wait for 50% with timeout
+        [InlineData(100, 0, 0, 50)]   // Wait for 100% (full brightness)
+        public async Task WaitForPercentageAsync_ImmediateReturnTrueWhenAlreadyInState(byte percentage, int waitingTime, int executionTimeMin, int executionTimeMax)
+        {
+            // TODO: Test WaitForPercentageAsync: immediate return when already at target percentage
+            // Parameters: percentage, waitingTime, executionTimeMin, executionTimeMax
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Theory]
+        [InlineData(0, 100, 50, 200, 50, true, 180, 220)]   // Wait for 50% from 0%, state changes
+        [InlineData(100, 50, 0, 200, 0, true, 180, 220)]    // Wait for 0% from 100%, state changes
+        [InlineData(50, 25, 75, 50, 75, false, 40, 70)]     // Wait for 75% from 50%, timeout
+        public async Task WaitForPercentageAsync_ShouldReturnCorrectly(byte initialPercentage, byte targetPercentage, byte feedbackPercentage, int waitingTime, byte expectedPercentage, bool expectedResult, int executionTimeMin, int executionTimeMax)
+        {
+            // TODO: Test WaitForPercentageAsync with various scenarios
+            // Parameters: initialPercentage, targetPercentage, feedbackPercentage, waitingTime, expectedPercentage, expectedResult, executionTimeMin, executionTimeMax
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Theory]
+        [InlineData(0, 50, 50, 200, 50, 100, 200)]     // Wait for 50% from 0%
+        [InlineData(100, 0, 75, 200, 0, 125, 225)]     // Wait for 0% from 100%
+        [InlineData(50, 25, 100, 200, 25, 150, 250)]   // Wait for 25% from 50%
+        public async Task WaitForPercentageAsync_WhenFeedbackReceived_ShouldReturnTrue(byte initialPercentage, byte targetPercentage, int delayInMs, int waitingTime, byte expectedPercentage, int executionTimeMin, int executionTimeMax)
+        {
+            // TODO: Test that wait method returns true when feedback changes percentage to target
+            // Parameters: initialPercentage, targetPercentage, delayInMs, waitingTime, expectedPercentage, executionTimeMin, executionTimeMax
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        #endregion
+
+        #region Dimmer-Specific Command Tests
+
+        [Theory]
+        [InlineData(Lock.Off)] // When unlocked
+        [InlineData(Lock.On)]  // When locked (should unlock first)
+        public async Task SetPercentageAsync_WhenLocked_ShouldUnlockThenSetPercentage(Lock lockState)
+        {
+            // TODO: Test that SetPercentageAsync unlocks device before setting percentage if locked
+            // Parameter: lockState
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Theory]
+        [InlineData(95, 5, 100)] // Increase to max
+        [InlineData(5, 10, 15)]  // Normal increase
+        [InlineData(90, 20, 100)] // Increase with clamping to max
+        public async Task IncreaseBrightnessAsync_ShouldNotExceedMaximum(byte currentPercentage, byte increment, byte expectedResult)
+        {
+            // TODO: Test that IncreaseBrightnessAsync clamps result to maximum 100%
+            // Parameters: currentPercentage, increment, expectedResult
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        [Theory]
+        [InlineData(5, 5, 0)]   // Decrease to min
+        [InlineData(15, 10, 5)] // Normal decrease
+        [InlineData(10, 20, 0)] // Decrease with clamping to min
+        public async Task DecreaseBrightnessAsync_ShouldNotGoBelowMinimum(byte currentPercentage, byte decrement, byte expectedResult)
+        {
+            // TODO: Test that DecreaseBrightnessAsync clamps result to minimum 0%
+            // Parameters: currentPercentage, decrement, expectedResult
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        #endregion
+
+        #region Dimmer-Specific Edge Cases Tests
+
+        [Theory]
+        [InlineData(150)] // Above 100%
+        [InlineData(200)] // Way above 100%
+        public void InvalidPercentageFeedback_ShouldBeHandledGracefully(byte invalidPercentage)
+        {
+            // TODO: Test that invalid percentage feedback is handled gracefully
+            // Parameter: invalidPercentage
+            throw new NotImplementedException("Test not implemented yet");
+        }
+
+        #endregion
+
+        #region Interface Composition Tests
+
+        [Fact]
+        public void DimmerDevice_ImplementsAllRequiredInterfaces()
+        {
+            // Assert
+            _device.Should().BeAssignableTo<IKnxDeviceBase>();
+            _device.Should().BeAssignableTo<ISwitchable>();
+            _device.Should().BeAssignableTo<IPercentageControllable>();
+            _device.Should().BeAssignableTo<ILockableDevice>();
+            _device.Should().BeAssignableTo<IDimmerDevice>();
+        }
+
+        #endregion
     }
 }
